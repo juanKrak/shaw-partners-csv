3b4123866a2ded55a1acb27f9962afeb1ed25c22
juanKrak <juan@krak.com.ar>
2024-01-28 05:09:15 -0300

    added a footer (amazing)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Upload } from './layouts/Upload.tsx'
 import { Query } from './layouts/Query.tsx'
 import { create } from 'zustand'
+import Logo from './components/Logo.tsx'
 
 interface storeState {
     queryFlag: boolean,
@@ -15,8 +16,21 @@ function App() {
 
     return (
         <main>
+            <header className="max-w-6xl pt-4 px-4 lg:px-0 lg:py-16 absolute top-0 left-0 right-0 w-full mx-auto">
+                <Logo/>
+            </header>
             <Upload/>
             <Query/>
+            <footer className='w-full bg-gray-800 text-gray-300 py-8 px-4 lg:px-0 lg:py-32 rounded-t-xl'>
+                <div className="max-w-6xl w-full mx-auto grid lg:grid-cols-3">
+                    <Logo dark={true} />
+                    <div className="my-4" />
+                    <div>
+                        <h1 className='text-lg lg:text-2xl font-medium mb-4'><span className='text-xl lg:text-3xl italic font-bold'>"</span>A carefully handcrafted product.<span className='text-xl lg:text-3xl italic font-bold'>"</span></h1>
+                        <h2>By <a href="mailto:juan@krak.com.ar" className='text-violet-500 underline font-bold underline-offset-2'>Juan Rolon</a>.</h2>
+                    </div>
+                </div>                    
+            </footer>
         </main>
     )
 }
